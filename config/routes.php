ad0ac834fe7c1bfe061556bd61b0d492b9f4bc4d
Serge <moiserge.k@gmail.com>
2017-12-24 23:33:05 +0300

    +

--- config/routes.php
+++ config/routes.php
@@ -9,27 +9,16 @@ use App\Core\Common\Route;
  */
 
 Route::GET("/")->redirectTo("/projects/commercial");
-
 Route::GET("/404")->handleBy("home", "_404");
-
 Route::POST("/apply_filters")->handleBy("home", "applyFilter");
-
 Route::GET("/projects/commercial")->handleBy("home", "index");
-
 Route::GET("/projects/my")->handleBy("home", "myProjects");
-
 Route::GET("/login")->handleBy("auth", "index");
-
 Route::GET("/try_login")->handleBy("auth", "login");
-
 Route::GET("/logout")->handleBy("auth", "logout");
-
 Route::GET("/get_card_info")->handleBy("home", "getCardInfo");
-
-Route::POST("/addmail")->handleBy("home", "addMail");
-
+Route::POST("/mail/add")->handleBy("home", "addMail");
 Route::GET("/logger")->handleBy("logger", "index");
-
 Route::GET("/logger/add")->handleBy("logger", "addMessage");
 
 /*
@@ -37,19 +26,11 @@ Route::GET("/logger/add")->handleBy("logger", "addMessage");
  */
 
 Route::GET("/admin")->if(Route::auth())->handleBy("admin", "index")->else()->redirectTo("/");
-
 Route::GET('/admin/projects')->if(Route::auth())->handleBy("admin", "projects")->else()->redirectTo("/");
-
 Route::POST('/admin/projects/add')->if(Route::auth())->handleBy("admin", "addProject")->else()->redirectTo("/");
-
 Route::POST('/admin/projects/update')->if(Route::auth())->handleBy("admin", "updateProject")->else()->redirectTo("/");
-
 Route::POST('/admin/projects/delete')->if(Route::auth())->handleBy("admin", "deleteProject")->else()->redirectTo("/");
-
 Route::POST('/admin/projects/edit')->if(Route::auth())->handleBy("admin", "editProject")->else()->redirectTo("/");
-
 Route::GET('/admin/mails')->if(Route::auth())->handleBy("admin", "mails")->else()->redirectTo("/");
-
 Route::GET('/admin/mails/delete')->if(Route::auth())->handleBy("admin", "deleteMail")->else()->redirectTo("/");
-
 Route::GET('/admin/tags')->if(Route::auth())->handleBy("admin", "tags")->else()->redirectTo("/");
--- public/views/_modals/mail.php
+++ public/views/_modals/mail.php
@@ -3,7 +3,7 @@
         <div class="container">
             <button id="mailCloseBtn"><b>x</b></button>
             <h3 class="hr-bottom hr-dark text-left">Mail me</h3>
-            <form method="post" action="/addmail" name="save_project_form" enctype="multipart/form-data">
+            <form method="post" action="/mail/add" name="save_project_form" enctype="multipart/form-data">
                 <input placeholder="Your name..." name="name">
                 <input placeholder="Your@email..." name="email">
                 <textarea class="materialize-textarea" data-length="120" name="message" placeholder="Your message..."></textarea>
